6a6d8c6f7e87b375e14aaecb8ff715fc2551a726
Andrey Bogatyrev <t2way@yandex.ru>
2022-10-31 01:05:27 +0300

    update is_even.py
    refactoring: add greeting(), add get_valid_answer(num)

--- brain_games/is_even.py
+++ brain_games/is_even.py
@@ -1,6 +1,13 @@
 from random import randint
 
 
+def greeting():
+
+    name = input('May I have your name? ')
+    print(f'Hello, {name}!')
+    return name
+
+
 def get_number():
 
     number = randint(-1000000, 1000000)
@@ -16,40 +23,43 @@ def is_even_number(num):
     return answer
 
 
-def run_game():
-
-    name = input('May I have your name? ')
-    print(f'Hello, {name}!')
+def get_valid_answer(num):
 
     valid_answers = ('yes', 'no')
+
+    while True:
+        answer = input(f'Question: {num}: ')
+
+        if answer in valid_answers:
+            break
+        else:
+            print("Your answer isn't valid, type 'yes' or 'no'")
+    return answer
+
+
+def run_game():
+
     task = "Answer 'yes' if the number is even, otherwise answer 'no'."
     count = 0
-
+    user_name = greeting()
     print(task)
 
     while count < 3:
 
         current_number = get_number()
         right_answer = is_even_number(current_number)
-
-        while True:
-            user_answer = input(f'Question: {current_number}: ')
-
-            if user_answer in valid_answers:
-                break
-            else:
-                print("Your answer isn't valid, type 'yes' or 'no'")
+        user_answer = get_valid_answer(current_number)
 
         if user_answer == right_answer:
             print('Correct!')
             count += 1
         else:
             print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{right_answer}'.")
-            print(f"Let's try again, {name}!")
+            print(f"Let's try again, {user_name}!")
             break
 
     if count == 3:
-        print(f'Congratulations, {name}!')
+        print(f'Congratulations, {user_name}!')
 
 
 run_game()
