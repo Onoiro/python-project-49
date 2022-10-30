1805527551d58a95ed59a5a60430477cbea16b11
Andrey Bogatyrev <t2way@yandex.ru>
2022-10-30 10:55:26 +0300

    update brain_even.py with def main()

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,37 +1,48 @@
+#!/usr/bin/env python3
+
 from random import randint
 
-print("Welcome to the Brain Games!")
-name = input('May I have your name? ')
-user_answer = ''
-right_answer = ''
-valid_answers = ('yes', 'no')
-task = "Answer 'yes' if the number is even, otherwise answer 'no'."
-count = 0
+def main():
 
-print(task)
+    print("Welcome to the Brain Games!")
+    name = input('May I have your name? ')
+    print(f'Hello, {name}!')
+    user_answer = ''
+    right_answer = ''
+    valid_answers = ('yes', 'no')
+    task = "Answer 'yes' if the number is even, otherwise answer 'no'."
+    count = 0
 
-while count < 3:
+    print(task)
 
-    number = randint(-1000000, 1000000)
+    while count < 3:
 
-    if number%2 == 0:
-        right_answer = 'yes'
-    else:
-        right_answer = 'no'
+        number = randint(-1000000, 1000000)
 
-    while True:
-        user_answer = input(f'Question: {number}: ')
-
-        if user_answer in valid_answers:
-            break
+        if number%2 == 0:
+            right_answer = 'yes'
         else:
-            print("Your answer isn't valid, type 'yes' or 'no'")
-    
-    if user_answer == right_answer:
-        count += 1
-    else:
-        print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{right_answer}'.\nLet's try again, {name}!")
-        break
-    
-if count == 3:
-    print(f'Congratulations, {name}!')
+            right_answer = 'no'
+
+        while True:
+            user_answer = input(f'Question: {number}: ')
+
+            if user_answer in valid_answers:
+                break
+            else:
+                print("Your answer isn't valid, type 'yes' or 'no'")
+        
+        if user_answer == right_answer:
+            print('Correct!')
+            count += 1
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{right_answer}'.")
+            print(f"Let's try again, {name}!")
+            break
+        
+    if count == 3:
+        print(f'Congratulations, {name}!')
+
+
+if __name__ == '__main__':
+    main()
