5b5a0ec9bbf76b417b15388f490b719afb227340
Andrey Bogatyrev <t2way@yandex.ru>
2022-11-15 18:03:49 +0300

    update brain_even.py and brain_prime.py
    modify is_even() and is_prime() to return True or False

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -4,16 +4,17 @@ from random import randint
 def is_even_number(num):
 
     if num % 2 == 0:
-        answer = 'yes'
-    else:
-        answer = 'no'
-    return answer
+        
+        return True
 
 
 def build_task():
 
     task = 'Answer "yes" if the number is even, otherwise answer "no".'
     current_number = randint(-100, 100)
-    right_answer = is_even_number(current_number)
+    if is_even_number(current_number) is True:
+        right_answer = 'yes'
+    else:
+        right_answer = 'no'
 
     return right_answer, current_number, task
--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -6,18 +6,19 @@ def is_prime_number(num):
     divider = 2
     while divider < num:
         if num % divider == 0:
-            answer = 'no'
-            break
+            return False
         divider += 1
-        answer = 'yes'
-
-    return answer
+    
+    return True
 
 
 def build_task():
 
     task = 'Answer "yes" if given number is prime. Otherwise answer "no".'
-    current_number = randint(1, 100)
-    right_answer = is_prime_number(current_number)
+    current_number = randint(3, 100)
+    if is_prime_number(current_number) is True:
+        right_answer = 'yes'
+    else:
+        right_answer = 'no'
 
     return right_answer, current_number, task
