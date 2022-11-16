c6e8260f4a11d618fe3e07a048319da378ea3dc9
Andrey Bogatyrev <t2way@yandex.ru>
2022-11-16 13:06:46 +0300

    update brain_prime.py
    from is_prime_number() return 'yes' or 'no' instead True or False

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -6,19 +6,18 @@ def is_prime_number(num):
     divider = 2
     while divider < num:
         if num % divider == 0:
-            return False
+            answer = 'no'
+            return answer
         divider += 1
+        answer = 'yes'
 
-    return True
+    return answer
 
 
 def build_task():
 
     task = 'Answer "yes" if given number is prime. Otherwise answer "no".'
     current_number = randint(3, 100)
-    if is_prime_number(current_number) is True:
-        right_answer = 'yes'
-    else:
-        right_answer = 'no'
+    right_answer = is_prime_number(current_number)
 
     return right_answer, current_number, task
