7cb060529dcae53f5d05703281e2797e0f7795ac
Andrey Bogatyrev <t2way@yandex.ru>
2022-11-18 19:57:42 +0300

    update game_play.py - make only one func instead three

--- brain_games/game_play.py
+++ brain_games/game_play.py
@@ -2,34 +2,23 @@ import prompt
 
 
 def run_game(game):
-    user_name = greet_user()
+    print("Welcome to the Brain Games!")
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
     print(game.TASK)
     answers_count = 0
     while answers_count < 3:
         right_answer, task_value = game.build_task()
         user_answer = prompt.string(f'Question: {task_value}\nYour answer: ')
-        answers_count = print_messages(right_answer, user_answer,
-                                       user_name, answers_count)
+        if user_answer == right_answer:
+            message = 'Correct!'
+            answers_count += 1
+            if answers_count == 3:
+                message = f'Correct!\nCongratulations, {user_name}!'
+        else:
+            message = f"'{user_answer}' is wrong answer ;(. \
+    Correct answer was '{right_answer}'.\nLet's try again, {user_name}!"
+            answers_count = 0
+        print(message)
         if answers_count == 0:
             break
-
-
-def greet_user():
-    print("Welcome to the Brain Games!")
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    return name
-
-
-def print_messages(result, answer, name, count):
-    if answer == result:
-        message = 'Correct!'
-        count += 1
-        if count == 3:
-            message = f'Correct!\nCongratulations, {name}!'
-    else:
-        message = f"'{answer}' is wrong answer ;(. \
-Correct answer was '{result}'.\nLet's try again, {name}!"
-        count = 0
-    print(message)
-    return count
