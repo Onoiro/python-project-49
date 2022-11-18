51dfd1542bea20fe4bbf06037d05417a929d8ac0
Andrey Bogatyrev <t2way@yandex.ru>
2022-11-18 22:52:01 +0300

    update brain_even.py - make only one func

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -3,13 +3,9 @@ from random import randint
 TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def is_even(num):
-    return num % 2 == 0
-
-
 def build_task():
     number = randint(-100, 100)
-    if is_even(number):
+    if number % 2 == 0:
         right_answer = 'yes'
     else:
         right_answer = 'no'
