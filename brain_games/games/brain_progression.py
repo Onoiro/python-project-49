be1ac9b2768c60cb4a2c87bde63b2fc5d818b140
Andrey Bogatyrev <t2way@yandex.ru>
2022-11-18 21:08:12 +0300

    update brain_progression.py
    change var name current_progression to progression
    make var missing_number_index from 1 till len(progression)

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -5,7 +5,7 @@ TASK = "What number is missing in the progression?"
 
 def get_progression(first_number, prog_step):
     progression = []
-    for i in range(1, 10):
+    for i in range(10):
         prog_number = first_number + prog_step
         first_number = first_number + prog_step
         progression.append(str(prog_number))
@@ -13,11 +13,11 @@ def get_progression(first_number, prog_step):
 
 
 def build_task():
-    number1 = randint(1, 10)
-    number3 = randint(2, 10)
-    missing_number_index = randint(1, 8)
-    current_progression = get_progression(number1, number3)
-    right_answer = current_progression[missing_number_index]
-    current_progression[missing_number_index] = '..'
-    task_string = " ".join(current_progression)
+    start_prog_number = randint(1, 10)
+    step_prog_number = randint(1, 10)
+    progression = get_progression(start_prog_number, step_prog_number)
+    missing_number_index = randint(1, len(progression))
+    right_answer = progression[missing_number_index]
+    progression[missing_number_index] = '..'
+    task_string = " ".join(progression)
     return right_answer, task_string
