aa545b920199a285b7b6560958785b2b4a760d39
Andrey Bogatyrev <t2way@yandex.ru>
2022-11-18 20:07:57 +0300

    update brain_even - change var name
    current_number to number

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -3,14 +3,14 @@ from random import randint
 TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def is_even_number(num):
+def is_even(num):
     return num % 2 == 0
 
 
 def build_task():
-    current_number = randint(-100, 100)
-    if is_even_number(current_number) is True:
+    number = randint(-100, 100)
+    if is_even(number):
         right_answer = 'yes'
     else:
         right_answer = 'no'
-    return right_answer, current_number
+    return right_answer, number
