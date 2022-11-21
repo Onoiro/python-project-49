0a3464dc7fbd357777e100d973932f99d567985d
Andrey Bogatyrev <t2way@yandex.ru>
2022-11-21 10:34:55 +0300

    update game_play.py - add MAX_ANSWERS_COUNT = 3

--- brain_games/game_play.py
+++ brain_games/game_play.py
@@ -1,5 +1,7 @@
 import prompt
 
+MAX_ANSEWRS_COUNT = 3
+
 
 def run_game(game):
     print("Welcome to the Brain Games!")
@@ -7,13 +9,13 @@ def run_game(game):
     print(f'Hello, {user_name}!')
     print(game.TASK)
     answers_count = 0
-    while answers_count < 3:
+    while answers_count < MAX_ANSEWRS_COUNT:
         right_answer, task_value = game.build_task()
         user_answer = prompt.string(f'Question: {task_value}\nYour answer: ')
         if user_answer == right_answer:
             message = 'Correct!'
             answers_count += 1
-            if answers_count == 3:
+            if answers_count == MAX_ANSEWRS_COUNT:
                 message = f'Correct!\nCongratulations, {user_name}!'
         else:
             message = f"'{user_answer}' is wrong answer ;(. \
